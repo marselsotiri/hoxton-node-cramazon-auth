9c76911d2ead7155f1f0634bfd946d929ccd2b5a
Marsel <you@example.com>
2022-03-17 23:33:16 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,75 @@
+import express from 'express'
+import cors from 'cors'
+
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+const prisma = new PrismaClient({
+    log: ['error', 'info', 'query', 'warn']
+})
+
+
+function createToken(id: number) {
+    const token = jwt.sign({ id: id }, 'shhhh', { expiresIn: '3day' })
+    return token
+}
+
+async function getUserFromToken(token: string) {
+    const data = jwt.verify(token, 'shhhh')
+    //@ts-ignore
+    const user = await prisma.user.findUnique({ where: { id: data.id }, include: { orders: { include: { item: true } } } })
+    return user
+}
+
+app.post('/sign-up', async (req, res) => {
+    const { email, password, name } = req.body
+    try {
+        const hash = bcrypt.hashSync(password)
+        const user = await prisma.user.create({ data: { email, password: hash, name }, include: { orders: { include: { item: true } } } })
+        res.send({ user, token: createToken(user.id) })
+    } catch (err) {
+        //@ts-ignore
+        res.status(400).send({ error: err.message })
+    }
+})
+
+app.post('/sign-in', async (req, res) => {
+    const { email, password } = req.body
+    try {
+        //@ts-ignore
+        const user = await prisma.user.findUnique({ where: { email: email } })
+        //@ts-ignore
+        const passwordMatches = bcrypt.compareSync(password, user.password)
+        if (user && passwordMatches) {
+            res.send({ user, token: createToken(user.id) })
+        } else {
+            throw Error('Boom')
+        }
+    } catch (err) {
+        res.status(400).send({ error: 'Email/password invalid.' })
+    }
+
+})
+
+
+app.get('/validate', async (req, res) => {
+    const token = req.headers.authorization || ''
+    try {
+        const user = await getUserFromToken(token)
+        res.send(user)
+    } catch (err) {
+        //@ts-ignore
+        res.status(400).send({ error: err.message })
+    }
+
+})
+
+app.listen(4000, () => {
+    console.log(`Server up: http://localhost:4000`)
+})
---
+++ prisma/seed.ts
@@ -0,0 +1,78 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import bcrypt from 'bcryptjs'
+
+const prisma = new PrismaClient()
+
+const items: Prisma.ItemCreateInput[]= [
+    {
+        title: "T-Shirt",
+        image: "https://static.zajo.net/content/mediagallery/zajo_dcat/image/product/types/X/9088.png",
+        price: 15
+    },
+    {
+        title: "Trousers",
+        image: "https://www.7camicie.com/17969-big_default/trousers-chinos-premium-quality-beige-5788.jpg",
+        price: 20
+    },
+    {
+        title: "Shorts",
+        image: "https://www.haglofs.com/dw/image/v2/BBTN_PRD/on/demandware.static/-/Sites-master-catalog-haglofs/default/dw81fcf64b/hi-res/6037753C5_S20_2_m1_grey.jpg?sw=1500&sh=1500&sm=fit",
+        price: 35
+    }
+]
+const users: Prisma.UserCreateInput[] = [
+    {
+        email: "marsel@email.com",
+        password: bcrypt.hashSync('marsel'),
+        name: "Marsel",
+        orders: {
+            create: [
+                { item: { connect: { title: 'T-Shirt' } }, quantity: 100 },
+                { item: { connect: { title: 'Shorts' } }, quantity: 50 },
+                { item: { connect: { title: 'Trousers' } }, quantity: 20 }
+            ]
+        }
+    },
+    {
+        email: "aldo@email.com",
+        password: bcrypt.hashSync('aldo'),
+        name: "Aldo",
+        orders: {
+            create: [
+                {
+                    item: {
+                        connectOrCreate: {
+                            where: { title: 'Shoes' },
+                            create: { image: 'https://assets.ajio.com/medias/sys_master/root/hd4/h99/14092964397086/-1117Wx1400H-460455972-black-MODEL.jpg', title: 'Shoes', price: 10 }
+                        }
+                    },
+                    quantity: 39
+                }
+            ]
+        }
+    },
+    {
+        email: "ben@email.com",
+        password: bcrypt.hashSync('ben'),
+        name: "Ben",
+        orders: {
+            create: [
+                { item: { connect: { title: 'T-Shirt' } }, quantity: 60 },
+                { item: { connect: { title: 'Shorts' } }, quantity: 54 },
+                { item: { connect: { title: 'Trousers' } }, quantity: 14 }
+            ]
+        }
+    }
+]
+
+async function createStuff(){
+    for (const item of items){
+        await prisma.item.create({data: item})
+    }
+
+    for (const user of users){
+        await prisma.user.create({data: user})
+    }
+}
+
+createStuff()
